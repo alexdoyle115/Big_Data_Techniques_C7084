c05b495b88c934ee8c86925abbe4bde6ba0dae10
Alex D <alexdoyle115@gmail.com>
2021-04-15 11:16:58 +0100

    Create formatting_submission.R

---
+++ formatting_submission.R
@@ -0,0 +1,17 @@
+## Code Chunk for reordering submission .csv
+
+setwd("/Users/alexd/Desktop/C7084-Assignment/")
+
+test_data <- read.csv("test_values.csv") # loading in the file from the directory
+submission <- read.csv("results/submission.csv") # loading the file of the predicted statu_group
+
+
+id <- test_data %>%
+  select(id) %>%
+  left_join(submission, by = "id") %>%
+  replace_na(list(status_group = "functional")) # for some reason there are a number of NA's
+
+id[is.na(id)] # double checking there are no 'NA's
+  
+
+write.csv(id, "submission.csv") # rewrite the .csv for submission 
